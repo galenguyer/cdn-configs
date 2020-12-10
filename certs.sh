5711e551ec8f987f851c41147efcc3f312bab8c3
Galen Guyer <galen@galenguyer.com>
2020-12-09 23:22:33 -0500

    add certs script

---
+++ certs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# install certbot-auto if not present
+if ! command -v certbot-auto 2>&1 >/dev/null; then
+	info "certbot not installed, installing now"
+	sudo wget https://dl.eff.org/certbot-auto -O /usr/local/bin/certbot-auto
+	sudo chmod a+x /usr/local/bin/certbot-auto
+fi
+
+# get star and root cert
+sudo certbot-auto certonly \
+	--server https://acme-v02.api.letsencrypt.org/directory \
+	--manual \
+	--preferred-challenges dns \
+	-m certbot@galenguyer.com \
+	--rsa-key-size 4096 \
+	--agree-tos \
+	-d 'galenguyer.com' \
+	-d '*.galenguyer.com'
