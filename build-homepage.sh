0b6b71a67a06e6a40651013c344c1517d4119666
Galen Guyer <galen@galenguyer.com>
2020-12-13 05:54:31 -0500

    scripts: make build-homepage a bit quiet

--- build-homepage.sh
+++ build-homepage.sh
@@ -2,10 +2,10 @@
 
 git clone https://github.com/galenguyer/galenguyer.com
 cd galenguyer.com
-wget https://github.com/getzola/zola/releases/download/v0.12.2/zola-v0.12.2-x86_64-unknown-linux-gnu.tar.gz
-tar xzvf zola-v0.12.2-x86_64-unknown-linux-gnu.tar.gz
+wget -q https://github.com/getzola/zola/releases/download/v0.12.2/zola-v0.12.2-x86_64-unknown-linux-gnu.tar.gz
+tar xzf zola-v0.12.2-x86_64-unknown-linux-gnu.tar.gz
 ./zola build
-chown www-data:www-data public -Rv
+chown www-data:www-data public -R
 cd ../ansible
 ansible-playbook homepage.yaml
 cd ..
